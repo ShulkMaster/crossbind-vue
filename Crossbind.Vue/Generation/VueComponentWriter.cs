03478571e5dbc2edaf45fe88859d137ff3cab386
David Massana <00072416@uca.edu.sv>
2022-09-24 21:59:52 -0600

    :sparkles: Add vue component writer class

---
+++ Crossbind.Vue/Generation/VueComponentWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossbind_Vue.Generation;
+
+public class VueComponentWriter
+{
+    private readonly StringBuilder _sb;
+
+    public VueComponentWriter(StringBuilder sb)
+    {
+        _sb = sb;
+    }
+
+    public void WriteSourceCode()
+    {
+        WriteTemplateSection();
+        WriteScriptSection();
+        WriteStyleSection();
+    }
+
+    private void WriteTemplateSection()
+    {
+        _sb.AppendLine("<template>");
+        // TODO: Add write markup section
+        _sb.AppendLine("</template>");
+    }
+
+    private void WriteScriptSection()
+    {
+
+    }
+
+    private void WriteStyleSection()
+    {
+
+    }
+}
+
