ac464b03a62bb3173278bf02e9a9197c24c99f27
David Massana <00072416@uca.edu.sv>
2022-09-24 19:22:26 -0600

    Adding vue imports

---
+++ Crossbind.Vue/Imports/DefaultImport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossbind_Vue.Imports;
+
+public sealed class DefaultImport : VueImport
+{
+    public string ModuleName { get; init; } = string.Empty;
+
+    public DefaultImport(string path) : base(path)
+    {
+    }
+
+    public override void WriteImportStatement(StringBuilder sb, int indent)
+    {
+        sb.Append(' ', indent);
+        sb.Append($"import {ModuleName} from '{ModulePath}'");
+    }
+}
---
+++ Crossbind.Vue/Imports/VueImport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossbind_Vue.Imports;
+
+public abstract class VueImport
+{
+    public string ModulePath { get; set; }
+
+    protected VueImport(string modulePath)
+    {
+        ModulePath = modulePath;
+    }
+
+    public abstract void WriteImportStatement(StringBuilder sb, int indent);
+}
