713d0facea659ee8e421586c29c2fa9cb4b28fd4
David Massana <00072416@uca.edu.sv>
2022-09-25 21:07:31 -0600

    :sparkles: Add vue component definitions

---
+++ Crossbind.Vue/Component/VueComponent.cs
@@ -0,0 +1,12 @@
+﻿using CrossBind.Engine.BaseModels;
+using CrossBind.Engine.ComponentModels;
+
+namespace Crossbind_Vue.Component;
+
+// Vue component common definitions
+public class VueComponent
+{
+    public string componentName { get; init; } = string.Empty;
+    public List<PropModel> Props { get; init; } = new();
+    public ComponentModel Model { get; init; } = null!;
+}
--- Crossbind.Vue/Generation/VueComponentWriter.cs
+++ Crossbind.Vue/Generation/VueComponentWriter.cs
@@ -22,6 +22,7 @@ public class VueComponentWriter
         WriteStyleSection();
     }
 
+    #region Vue SFC Sections
     private void WriteTemplateSection()
     {
         _sb.AppendLine("<template>");
@@ -31,12 +32,17 @@ public class VueComponentWriter
 
     private void WriteScriptSection()
     {
-
+        _sb.AppendLine("<script>");
+        // TODO: Fill all the vue component config such as props, component name HERE
+        _sb.AppendLine("</script>");
     }
 
     private void WriteStyleSection()
     {
-
+        _sb.AppendLine("<style scoped>");
+        // TODO: Handle css imports here
+        _sb.AppendLine("</style");
     }
+    #endregion
 }
 
